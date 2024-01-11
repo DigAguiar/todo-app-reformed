6be9b9b24e166c3aa4f05baff8721cab591d756f
DigAguiar <diogobarreto2004@gmail.com>
2024-01-11 13:05:25 -0300

    feat: initial methods

--- src/main/java/br/todoappv2/controllers/TaskController.java
+++ src/main/java/br/todoappv2/controllers/TaskController.java
@@ -1,8 +1,12 @@
 package br.todoappv2.controllers;
 
+import br.todoappv2.entities.Task;
 import br.todoappv2.services.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,6 +14,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TaskController {
     @Autowired
     TaskService taskService;
+
+
+    @GetMapping("/list")
+    public String refreshPage (@ModelAttribute Task task) {
+
+    }
+
+
+    @PostMapping("/createTask")
+    public String createNewTask () {
+
+        taskService.saveNewTask();
+        return "index";
+    }
 
 
 }
--- src/main/java/br/todoappv2/services/TaskService.java
+++ src/main/java/br/todoappv2/services/TaskService.java
@@ -1,5 +1,6 @@
 package br.todoappv2.services;
 
+import br.todoappv2.entities.Task;
 import br.todoappv2.repositories.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,5 +9,23 @@ import org.springframework.stereotype.Service;
 public class TaskService {
     @Autowired
     TaskRepository taskRepository;
+
+
+    public String saveNewTask () {
+        Task task = new Task();
+        taskRepository.save(task);
+        return "task created!";
+    }
+
+    public void deleteTask (Long taskId) {
+        taskRepository.deleteById(taskId);
+    }
+
+    public void editTask (Long taskId,String taskDescription) {
+        Task task = taskRepository.getReferenceById(taskId);
+        task.setDescription(taskDescription);
+        taskRepository.save(task);
+    }
+
 
 }
