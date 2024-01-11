33802907fa4155cc23815c79b5ac576bcaf1fd54
DigAguiar <diogobarreto2004@gmail.com>
2024-01-11 11:50:46 -0300

    feat: first commit

---
+++ src/main/java/br/todoappv2/TodoappV2Application.java
@@ -0,0 +1,13 @@
+package br.todoappv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoappV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoappV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/todoappv2/TodoappV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.todoappv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
